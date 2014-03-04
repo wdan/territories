0c744aa8d7837972d94a730e4e143a412872f420
Yanhong Wu <wdan1990@gmail.com>
2014-03-05 06:35:48 +0800

    Add mds

---
+++ territories/models/mds.py
@@ -0,0 +1,32 @@
+__author__ = 'wenbin'
+
+import numpy as np
+from sklearn import manifold
+
+
+class MDS(object):
+
+    @classmethod
+    def cal_mds(cls, data, veryLarge):
+        n = len(data)
+        similarities = []
+        for i in xrange(n):
+            list = data[i]
+            tmp = [(veryLarge - j) for j in list]
+            similarities += tmp
+            #print similarities
+        similarities = np.array(similarities)
+        similarities = np.ndarray((n, n), buffer=similarities, dtype=int)
+        #print similarities
+        mds = manifold.MDS(n_components=2, max_iter=3000, eps=1e-9,
+                           dissimilarity="precomputed", n_jobs=1)
+        pos = mds.fit(similarities).embedding_
+        #print pos
+        return pos
+
+if __name__ == "__main__":
+    veryLarge = 400
+    data = [[veryLarge, 295, 74, 3, 6], [295, veryLarge, 82, 11, 13],
+            [74, 82, veryLarge, 0, 4], [3, 11, 0, veryLarge, 5],
+            [6, 13, 4, 5, veryLarge]]
+    print MDS.cal_mds(data, veryLarge)

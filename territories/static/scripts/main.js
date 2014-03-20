ceb2101462582e8eb5d70820221bf5e29f31a855
Wenbin <wwbcarol@gmail.com>
2014-03-21 02:00:16 +0800

    ready to merge

--- territories/static/scripts/main.js
+++ territories/static/scripts/main.js
@@ -13,6 +13,7 @@ var detail_svg;
 var gui;
 var detail;
 var merge_number;
+var orig_cluster_number;
 
 $('#dataTypeList').change(function(){
     var val = $('#dataTypeList').val();
@@ -30,6 +31,8 @@ $('#dataTypeList').change(function(){
             .attr('height', height);
         dataManager.getPolygon(val, width, height, 1);
         dataManager.getConstraints();
+        orig_cluster_number = dataManager.polygon.length;
+        console.log('orig:'+orig_cluster_number);
         dataManager.getDetailed();
 //        dataManager.getOriginal();
         dataManager.getClusterAttr();
@@ -43,21 +46,22 @@ $('#dataTypeList').change(function(){
         var voronoi = new LG.visual.Voronoi(gui, overview_svg, dataManager, sandBox, 'voronoi');
         sandBox.add('voronoi', voronoi);
 
-        var river_node = new LG.visual.RiverNode(gui, overview_svg, dataManager, sandBox, 'river_node');
-        sandBox.add('river_node', river_node);
-
-        if(detail_svg!=undefined){
-            d3.select('#detailed_view').select('svg').data([]).exit().remove();
-        }
-        detail_svg = d3.select('#detailed_view').append('svg')
-            .attr('width', 250)
-            .attr('height', 600);
-        detail = new LG.visual.DetailedView(gui, detail_svg, dataManager, sandBox, 'detailed_view');
-        sandBox.add('detailed_view', detail);
+//        var river_node = new LG.visual.RiverNode(gui, overview_svg, dataManager, sandBox, 'river_node');
+//        sandBox.add('river_node', river_node);
+//
+//        if(detail_svg!=undefined){
+//            d3.select('#detailed_view').select('svg').data([]).exit().remove();
+//        }
+//        detail_svg = d3.select('#detailed_view').append('svg')
+//            .attr('width', 250)
+//            .attr('height', 600);
+//        detail = new LG.visual.DetailedView(gui, detail_svg, dataManager, sandBox, 'detailed_view');
+//        sandBox.add('detailed_view', detail);
 
         voronoi.display();
-        river_node.display();
-//
+//        river_node.display();
+
+
 //        var boundary_node = new LG.visual.BoundaryNode(gui, svg, dataManager, 'boundary_node');
 //        boundary_node.display();
 //
@@ -73,30 +77,33 @@ $('#dataTypeList').change(function(){
     }
 });
 
-$('#update_cluster').click(function(){
-    if(sandBox.exchangeCluster.length==2){
-        dataManager.getNewPosition(sandBox.exchangeCluster);
-        dataManager.getConstraints();
-        sandBox.update_data_All();
-        sandBox.clearClusterQueue();
-    }else{
-        console.log('[WARNING] Please select two groups.');
-    }
-});
+//$('#update_cluster').click(function(){
+//    if(sandBox.exchangeCluster.length==2){
+//        dataManager.getNewPosition(sandBox.exchangeCluster);
+//        dataManager.getConstraints();
+////        sandBox.update_data_All();
+//            voronoi.update_data(dataManager.polygon);
+//        sandBox.clearClusterQueue();
+//    }else{
+//        console.log('[WARNING] Please select two groups.');
+//    }
+//});
 
-$('#show_cluster').click(function(){
-    if(sandBox.exchangeCluster.length==2){
-        sandBox.addDetail();
-        sandBox.clearClusterQueue();
-    }else{
-        console.log('[WARNING] Please select two groups.');
-    }
-});
+//$('#show_cluster').click(function(){
+//    if(sandBox.exchangeCluster.length==2){
+//        sandBox.addDetail();
+//        sandBox.clearClusterQueue();
+//    }else{
+//        console.log('[WARNING] Please select two groups.');
+//    }
+//});
 
 $('#merge_cluster').click(function(){
     var merge = sandBox.getMergeQueue();
     if(merge.length>=2){
-        dataManager.sendMergeRequest(merge, merge_number++);
+        dataManager.sendMergeRequest(merge, merge_number);
+        sandBox.merge_data('voronoi', dataManager.merge_info, merge, orig_cluster_number + merge_number - 1);
+        merge_number += 1;
     }else{
         console.log('[WARNING] Please select more than two groups.');
     }

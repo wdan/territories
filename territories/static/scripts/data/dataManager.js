22f9087e2d4082421834a8d2b88e7d5d59bb5885
Wenbin <wwbcarol@gmail.com>
2014-03-20 23:59:36 +0800

    add vonoroi selection and merge POST

--- territories/static/scripts/data/dataManager.js
+++ territories/static/scripts/data/dataManager.js
@@ -13,10 +13,10 @@ LG.data.DataManager = function(){
         this.width = undefined;
         this.height = undefined;
         this.rate = undefined;
-        this.visibleBoundaryNode = undefined;
-        this.externalBoundaryNode = undefined;
-        this.boundaryEdge = undefined;
-        this.insideNode = undefined;
+//        this.visibleBoundaryNode = undefined;
+//        this.externalBoundaryNode = undefined;
+//        this.boundaryEdge = undefined;
+//        this.insideNode = undefined;
         this.clusterAttr = {};
         this.max_degree_dict = {};
         this.max_degree = undefined;
@@ -56,6 +56,28 @@ LG.data.DataManager = function(){
                     if(res.length==2)break;
                 }
                 return res;
+            }
+        },
+
+        sendMergeRequest : {
+            value : function(cluster_list, merge_number){
+                var _this = this;
+                var startTime = new Date().getTime();
+                console.log('[LOG] Send Merge Request');
+                console.log('[POST] /send_merge?' + cluster_list + '&' + merge_number);
+                $.ajax({
+                    url: '/send_merge',
+                    data: {'cluster_list':cluster_list,'merge_number':merge_number},
+                    traditional: true,
+                    type: 'POST',
+                    success: function(data){
+                        var endTime = new Date().getTime();
+                        _this.merge_info = data;
+                        console.log('[LOG] Data Transmission Done. Used ' + (endTime-startTime)/1000 + 's');
+                        console.log(data);
+                    }
+
+                });
             }
         },
 
@@ -175,93 +197,93 @@ LG.data.DataManager = function(){
                 });
 
             }
-        },
-
-        getOriginal : {
-            value : function(){
-                var _this = this;
-                var startTime = new Date().getTime();
-                console.log('[URL] /get_original');
-                $.ajax({
-                    url: '/get_original',
-                    dataType: 'json',
-                    async: false,
-                    success: function(data){
-                        var endTime = new Date().getTime();
-                        _this.original = data;
-                        _this.visibleBoundaryNode = undefined;
-                        _this.externalBoundaryNode = undefined;
-                        _this.boundaryEdge = undefined;
-                        _this.insideNode = undefined;
-                        console.log('[LOG] Data Transmission Done. Used ' + (endTime-startTime)/1000 + 's');
-                        console.log(data);
-                    }
-                });
-            }
-        },
-
-        getExternalBoundaryNode : {
-
-            value : function(){
-
-                if (this.externalBoundaryNode == undefined){
-                    var node = [];
-                    var node_list = this.original.nodes;
-                    for(var i=0;i<node_list.length;i++){
-                        if(node_list[i]['external'] == 1 && node_list[i].x != undefined) node.push(node_list[i]);
-                    }
-                    this.externalBoundaryNode = node;
-                }
-
-                return this.externalBoundaryNode;
-            }
-
-        },
-
-        getVisibleBoundaryNode : {
-            value : function(){
-                if (this.visibleBoundaryNode == undefined){
-                    var node = [];
-                    var node_list = this.original.nodes;
-                    for(var i=0;i<node_list.length;i++){
-                        if(node_list[i]['external'] == 1 && node_list[i].visible == 1) node.push(node_list[i]);
-                    }
-                    this.visibleBoundaryNode = node;
-                }
-
-                return this.visibleBoundaryNode;
-            }
-        },
-
-        getEdge : {
-            value : function(){
-                if (this.boundaryEdge == undefined){
-                    var edge = [];
-                    var edge_list = this.original.links;
-                    for(var i=0;i<edge_list.length;i++){
-                        if(edge_list[i].visible == 1) edge.push(edge_list[i]);
-                    }
-                    this.boundaryEdge = edge;
-                }
-
-                return this.boundaryEdge;
-            }
-        },
-
-        getInsideNode : {
-            value : function(){
-                if (this.insideNode == undefined){
-                    var node = [];
-                    var node_list = this.original.nodes;
-                    for(var i=0;i<node_list.length;i++){
-                        if(node_list[i]['external'] == 0) node.push(node_list[i]);
-                    }
-                    this.insideNode = node;
-                }
-
-                return this.insideNode;
-            }
         }
+
+//        getOriginal : {
+//            value : function(){
+//                var _this = this;
+//                var startTime = new Date().getTime();
+//                console.log('[URL] /get_original');
+//                $.ajax({
+//                    url: '/get_original',
+//                    dataType: 'json',
+//                    async: false,
+//                    success: function(data){
+//                        var endTime = new Date().getTime();
+//                        _this.original = data;
+//                        _this.visibleBoundaryNode = undefined;
+//                        _this.externalBoundaryNode = undefined;
+//                        _this.boundaryEdge = undefined;
+//                        _this.insideNode = undefined;
+//                        console.log('[LOG] Data Transmission Done. Used ' + (endTime-startTime)/1000 + 's');
+//                        console.log(data);
+//                    }
+//                });
+//            }
+//        },
+//
+//        getExternalBoundaryNode : {
+//
+//            value : function(){
+//
+//                if (this.externalBoundaryNode == undefined){
+//                    var node = [];
+//                    var node_list = this.original.nodes;
+//                    for(var i=0;i<node_list.length;i++){
+//                        if(node_list[i]['external'] == 1 && node_list[i].x != undefined) node.push(node_list[i]);
+//                    }
+//                    this.externalBoundaryNode = node;
+//                }
+//
+//                return this.externalBoundaryNode;
+//            }
+//
+//        },
+//
+//        getVisibleBoundaryNode : {
+//            value : function(){
+//                if (this.visibleBoundaryNode == undefined){
+//                    var node = [];
+//                    var node_list = this.original.nodes;
+//                    for(var i=0;i<node_list.length;i++){
+//                        if(node_list[i]['external'] == 1 && node_list[i].visible == 1) node.push(node_list[i]);
+//                    }
+//                    this.visibleBoundaryNode = node;
+//                }
+//
+//                return this.visibleBoundaryNode;
+//            }
+//        },
+//
+//        getEdge : {
+//            value : function(){
+//                if (this.boundaryEdge == undefined){
+//                    var edge = [];
+//                    var edge_list = this.original.links;
+//                    for(var i=0;i<edge_list.length;i++){
+//                        if(edge_list[i].visible == 1) edge.push(edge_list[i]);
+//                    }
+//                    this.boundaryEdge = edge;
+//                }
+//
+//                return this.boundaryEdge;
+//            }
+//        },
+//
+//        getInsideNode : {
+//            value : function(){
+//                if (this.insideNode == undefined){
+//                    var node = [];
+//                    var node_list = this.original.nodes;
+//                    for(var i=0;i<node_list.length;i++){
+//                        if(node_list[i]['external'] == 0) node.push(node_list[i]);
+//                    }
+//                    this.insideNode = node;
+//                }
+//
+//                return this.insideNode;
+//            }
+//        }
 
     });
 
